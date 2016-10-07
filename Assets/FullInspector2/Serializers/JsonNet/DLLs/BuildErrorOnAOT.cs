177983dd149f67c5969f08b864bcc655f4b8d611
Jacob Dufault <jacobdufault@gmail.com>
2016-10-06 17:30:22 -0700

    Do not include Json.NET as a default serializer

--- Assets/FullInspector2/Serializers/JsonNet/DLLs/BuildErrorOnAOT.cs
+++
@@ -1,3 +0,0 @@
-﻿#if UNITY_IOS || UNITY_WEBPLAYER || UNITY_WINRT
-#error The default Json.NET implementation does not support this export platform. Please open up the serializer manager to remove it. You can open the serializer manager by clicking the menu item "Window/Full Inspector/Developer/Show Serializer Manager"; switch to the desktop export if you do not see the menu item.
-#endif
--- Assets/FullInspector2/Serializers/JsonNet/DLLs/README.txt
+++
@@ -1,7 +0,0 @@
-## Json.NET Customizations
-
-The build of Json.NET that Full Inspector ships with has been slightly customized; the dependency on sockets has been stripped.
-
-The files used to build Json.NET are available in `Newtonsoft.Json Build Files.zip`.
-
-If you have installed the alternative Json.NET package, you can safely delete this directory.
