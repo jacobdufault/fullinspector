2b4c7a61f548ef309816c0f01a3d946b95c6b281
SugoiDev <SugoiDev@lostalloy.com>
2016-11-15 03:42:14 -0200

    Added InspectorCurveAttribute

---
+++ Assets/FullInspector2/Modules/Editor/InspectorCurveAttributeEditor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+namespace FullInspector.Modules {
+    [CustomAttributePropertyEditor(typeof(InspectorCurveAttribute), ReplaceOthers = true)]
+    public class InspectorCurveAttributeEditor<TElement> : AttributePropertyEditor<TElement, InspectorCurveAttribute> {
+        private static T Cast<T>(object o) {
+            return (T)Convert.ChangeType(o, typeof(T));
+        }
+
+        protected override TElement Edit(Rect region, GUIContent label, TElement element, InspectorCurveAttribute attribute, fiGraphMetadata metadata) {
+            if (attribute.xMin >= attribute.xMax) {
+                Debug.Log("xMin cannot be larger than xMax. Resetting to 0 and 1");
+                attribute.xMin = 0;
+                attribute.xMax = 1;
+            }
+
+            if (attribute.yMin >= attribute.yMax) {
+                Debug.Log("yMin cannot be larger than yMax. Resetting to 0 and 1");
+                attribute.yMin = 0;
+               attribute.yMax =1;
+            }
+
+            var curveRange = new Rect(attribute.xMin, attribute.yMin, attribute.xMax, attribute.yMax);
+            var curve = Cast<AnimationCurve>(element);
+            return Cast<TElement>(EditorGUI.CurveField(region, label, curve, Color.green, curveRange));
+        }
+
+        protected override float GetElementHeight(GUIContent label, TElement element, InspectorCurveAttribute attribute, fiGraphMetadata metadata) {
+            return EditorStyles.largeLabel.CalcHeight(label, 100);
+        }
+
+        public override bool CanEdit(Type type) {
+            return type == typeof(AnimationCurve);
+        }
+    }
+}
---
+++ Assets/FullInspector2/Modules/InspectorCurveAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FullInspector {
+    /// <summary>
+    /// Keep a numeric value within the given min/max range, with an optional
+    /// step.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public sealed class InspectorCurveAttribute : Attribute {
+        public float xMin;
+        public float xMax;
+
+        public float yMin;
+        public float yMax;
+
+        public InspectorCurveAttribute(float xMin = 0, float yMin = 0, float xMax = 1, float yMax = 1) {
+            this.xMin = xMin;
+            this.xMax = xMax;
+            this.yMin = yMin;
+            this.xMax = xMax;
+        }
+    }
+}
