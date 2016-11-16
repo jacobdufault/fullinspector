45a70acfb5e5e1d515e0ceb05bcd329e573f25e9
SugoiDev <SugoiDev@lostalloy.com>
2016-11-15 22:04:59 -0200

    xml docs; added way to use in collections (using the default constructor).

--- Assets/FullInspector2/Modules/InspectorCurveAttribute.cs
+++ Assets/FullInspector2/Modules/InspectorCurveAttribute.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace FullInspector {
+
+
     /// <summary>
-    /// Keep a numeric value within the given min/max range, with an optional
-    /// step.
+    ///     Attribute for customized curves.<br />
+    ///     To use on collections, add <br />
+    ///     [<see cref="InspectorCollectionItemAttributesAttribute"/>(typeof(<see cref="InspectorMovementCurvesDefault"/>))]
+    ///     <br />
+    ///     Or create a custom class, similar to <see cref="InspectorMovementCurvesDefault" />
+    ///     instead.
     /// </summary>
     [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
     public sealed class InspectorCurveAttribute : Attribute {
@@ -14,10 +22,23 @@ namespace FullInspector {
         public float ValueEnd;
 
         public InspectorCurveAttribute(float timeStart = 0, float valueStart = 0, float timeEnd = 1, float valueEnd = 1) {
-            TimeStart = timeStart;
+            TimeStart = Mathf.Max(0, timeStart);
             TimeEnd = timeEnd;
             ValueStart = valueStart;
             ValueEnd = valueEnd;
         }
     }
+
+
+    /// <summary>
+    ///     Uses the default constructor from <see cref="InspectorCurveAttribute" /><br />
+    /// Usage:<br />
+    ///     [<see cref="InspectorCollectionItemAttributesAttribute"/>(typeof(<see cref="InspectorMovementCurvesDefault"/>))]
+    /// </summary>
+    public class InspectorMovementCurvesDefault: fiICollectionAttributeProvider {
+        public IEnumerable<object> GetAttributes() {
+            yield return new InspectorCurveAttribute();
+        }
+    }
+
 }
